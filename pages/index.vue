0cae2bc572093744907ed6f483367c7bf2e8dff4
Jet-Green <grishadzyin@gmail.com>
2025-05-19 20:57:53 +0500

    add text

--- pages/index.vue
+++ pages/index.vue
@@ -8,14 +8,31 @@ const userStore = useAuth();
     <v-row class="d-flex justify-center">
       <v-col cols="12" md="11" xl="10">
         <v-row>
-          <v-col cols="12">
-            <h1>Добро пожаловать</h1>
+          <v-col cols="12" xl="8">
+            <p class="mt-5 text-h4 font-weight-bold">
+              Шалом!
+            </p>
+            <p class="mt-5 text-h6 font-weight-medium">
+              Добро пожаловать в сообщество, влюбленных в Иврит.
+            </p>
+
+            <p class="mt-2 text-h6">
+              Здесь ты можешь найти идеального собеседника и партнера для языковой практики на основе своих предпочтений
+              и
+              интересов, завести новых друзей.
+            </p>
+            <p class="mt-2 text-h6">
+              Заполни <NuxtLink class="text-primary" to="/about-me-form">анкету</NuxtLink>, начни общение в чате и
+              продолжи на видеозвонке!
+            </p>
           </v-col>
+          <v-col cols="0" xl="4"></v-col>
+
           <v-col cols="12" md="4">
             <div class="card border">
               <v-row>
                 <v-col cols="12">
-                  <h2>Найдите партнера для изучения</h2>
+                  <p class="text-h5 font-weight-bold">Найдите партнера для изучения</p>
                   <p class="text-caption text-color-gray">Общайтесь и учитесь вместе с другими пользователями</p>
                 </v-col>
                 <v-col cols="12" class="d-flex justify-center" style="font-size: 50px;;">
@@ -31,7 +48,7 @@ const userStore = useAuth();
             <div class="card border">
               <v-row>
                 <v-col cols="12">
-                  <h2>Ваши сообщения</h2>
+                  <p class="text-h5 font-weight-bold">Ваши сообщения</p>
                   <p class="text-caption text-color-gray">Общайтесь и учитесь вместе с другими пользователями</p>
                 </v-col>
                 <v-col cols="12" class="d-flex justify-center" style="font-size: 50px;;">
@@ -47,7 +64,7 @@ const userStore = useAuth();
             <div class="card border">
               <v-row>
                 <v-col cols="12">
-                  <h2>Заполните анкету</h2>
+                  <p class="text-h5 font-weight-bold">Заполните анкету</p>
                   <p class="text-caption text-color-gray">Это поможет найти вам самый подходящий метч</p>
                 </v-col>
                 <v-col cols="12" class="d-flex justify-center" style="font-size: 50px;;">
@@ -63,7 +80,7 @@ const userStore = useAuth();
 
         <v-row id="candidates-list">
           <v-col cols="12">
-            <h1>Возможные партнеры</h1>
+            <p class="text-h4 font-weight-bold">Возможные партнеры</p>
           </v-col>
           <v-col cols="12">
             <CandidatesList />
