58d85eb0f152412b47f03fec217d1f5562e3ffaf
Jet-Green <grishadzyin@gmail.com>
2025-07-07 10:13:43 +0400

    add footer example

--- layouts/default.vue
+++ layouts/default.vue
@@ -25,8 +25,23 @@
       </v-container>
     </v-main>
 
-    <v-footer app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+    <v-footer>
+      <v-container>
+        <v-row class="d-flex justify-center align-center">
+          <v-col cols="12" md="11" xl="10" class="d-flex">
+            <v-row>
+              <v-col cols="6">
+                контакты и
+                copyright
+              </v-col>
+
+              <v-col cols="6" class="d-flex justify-end">
+                документы
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-footer>
   </v-app>
 </template>
