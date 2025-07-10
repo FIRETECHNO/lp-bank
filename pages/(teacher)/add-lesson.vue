529620de1887940f3eff8929491d62867a3a2b44
JetGreen <grishadzyin@gmail.com>
2025-07-10 16:56:25 +0400

    add-lesson page

---
+++ pages/(teacher)/add-lesson.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+<template>
+  <v-row class="d-flex justify-center align-center">
+    <v-col cols="12" md="11" xl="10">
+      добавить урок
+    </v-col>
+  </v-row>
+</template>
--- pages/teacher/my-lessons.vue
+++ pages/teacher/my-lessons.vue
@@ -4,5 +4,9 @@ definePageMeta({
 })
 </script>
 <template>
-
+  <v-row class="d-flex justify-center align-center">
+    <v-col cols="12" md="11" xl="10">
+      <v-btn to="/add-lesson">добавить урок</v-btn>
+    </v-col>
+  </v-row>
 </template>
