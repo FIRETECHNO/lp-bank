0db57db0ba1ea90d00ab7b354776384d2c775deb
JetGreen <grishadzyin@gmail.com>
2025-07-10 11:56:19 +0400

    display user

--- pages/parent/me.vue
+++ pages/parent/me.vue
@@ -2,11 +2,16 @@
 definePageMeta({
   layout: "parent-cabinet"
 })
+
+const authStore = useAuth();
+
+let user = authStore.user;
 </script>
 <template>
   <v-row class="d-flex justify-center align-center">
     <v-col cols="12" md="11" xl="10">
       parent me
+      {{ user }}
     </v-col>
   </v-row>
 </template>
