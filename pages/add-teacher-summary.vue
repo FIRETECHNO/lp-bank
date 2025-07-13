a292c2429691e470507c758f131db19dcbbe79a7
Poruch <139491864+Poruch@users.noreply.github.com>
2025-07-13 12:42:37 +0500

    add page add-teacher-summary

---
+++ pages/add-teacher-summary.vue
@@ -0,0 +1,106 @@
+<script lang="ts" setup>
+const items = [
+  {
+    name: "дошкольное образование",
+    department: "",
+  },
+  {
+    name: "начальное образование",
+    department: "1-4 класс",
+  },
+  {
+    name: "Основное общее образование",
+    department: "5-9 класс",
+  },
+  {
+    name: "Среднее общее образование",
+    department: "10-11 клкасс",
+  },
+  {
+    name: "Среднее профессиональное образование",
+    department: "Колледжи, техникумы",
+  },
+  {
+    name: "Высшее образование",
+    department: "Бакалавриат, специалитет, магистратура, аспирантура",
+  },
+];
+
+const expirienceTypes = ["до 1 года", "от 1 до 2 лет", "2-4 года", "4+ года"];
+let educationLevel = ref<String>("");
+let expirience = ref<String>("");
+let achievements = ref<String>("");
+let aboutMe = ref<String>("");
+
+function submit() {
+  console.log(educationLevel, expirience, achievements, aboutMe);
+}
+</script>
+
+<template>
+  <v-container>
+    <v-row class="d-flex justify-center align-center fill-height">
+      <v-col cols="12" sm="10" md="7" xl="6">
+        <span
+          class="d-flex flex-column justify-center align-center text-center rounded-lg w-100 pl-6 pr-6 pt-4 pb-6 text-h4"
+        >
+          Заполните анкету
+        </span>
+        <v-container>
+          <span class="text-h6"> Ваш уровень образования </span>
+          <v-select
+            :items="items"
+            item-title="name"
+            label="Уровень образования"
+            v-model="educationLevel"
+          >
+            <template v-slot:item="{ props: itemProps, item }">
+              <v-list-item
+                v-bind="itemProps"
+                :subtitle="item.raw.department"
+              ></v-list-item>
+            </template>
+          </v-select>
+          <span class="text-h6">
+            Ваш опыт работы репетитором или учителем наставником</span
+          >
+          <v-select
+            :items="expirienceTypes"
+            label="Опыт работы"
+            v-model="expirience"
+          >
+          </v-select>
+        </v-container>
+        <v-container fluid>
+          <span class="text-h6"> О вас </span>
+          <v-textarea
+            clear-icon="mdi-close-circle"
+            label="Опишите себя"
+            v-model="aboutMe"
+            clearable
+          ></v-textarea>
+        </v-container>
+        <v-container fluid>
+          <span class="text-h6"> Ваши достижения </span>
+          <v-textarea
+            clear-icon="mdi-close-circle"
+            label="Опишите свои достижения"
+            v-model="achievements"
+            clearable
+          ></v-textarea>
+        </v-container>
+        <v-container
+          class="d-flex mt-3 flex-column align-center justify-center w-100"
+        >
+          <v-btn
+            type="submit"
+            class="d-flex justify-center align-center text-center rounded-lg pl-6 pr-6 pt-4 pb-6"
+            @click="submit"
+          >
+            Отправить
+          </v-btn>
+        </v-container>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
